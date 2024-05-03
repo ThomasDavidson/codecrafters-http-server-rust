1fa3228ea29f06b9bc42f3e66982e0b266e569c2
ThomasDavidson <Tdavidson7992@conestogac.on.ca>
2024-05-02 23:42:01 -0400

    clean up code

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,11 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                // let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-                // let mut stream_clone = stream.try_clone().expect("clone failed...");
-
                 thread::spawn(move || {
                     let Some(request) = Request::new(&mut stream) else {
                         stream
